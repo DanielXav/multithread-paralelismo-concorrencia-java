d6b70369d53116cc9fb21f67258347cab481a687
Daniel Xavier <dxavier194@gmail.com>
2024-04-17 16:57:30 -0300

    Synchronized

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // Thread atual
-        Thread t = Thread.currentThread();
-        System.out.println(t.getName());
 
-        // Nova thread
-        Thread t1 = new Thread(new MeuRunnable());
-        // t1.run(); // Executando na mesma thread
-        t1.start(); // Executando em uma nova thread
-
-        // Runnable como lambda
-        Thread t2 = new Thread(() ->
-            System.out.println(Thread.currentThread().getName()));
-        t2.start();
-        // t2.start(); Iniciar duas vezes lança exceção
-
-        // Várias threads
-        Thread t3 = new Thread(new MeuRunnable());
-        t3.start();
     }
 }
